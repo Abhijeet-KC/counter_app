4ff0ed2a7f704252ac601e2e32598b32578637c7
abhi <abhijeetkc01@gmail.com>
2024-05-18 10:23:16 +0545

    img made better

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,16 +4,28 @@ class Counter extends Component {
     state = {
         count: 0,
         imageUrl: 'https://picsum.photos/200',
+        tag: ['tag1', 'tag2', 'tag3']
     };
-     
+    
+    style = { 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '2px'
+    };
+
     render() {
         let classes ="badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
 
         return (<React.Fragment>
-        <img src={this.state.imageUrl} alt=""/>
+        <img style = {this.style} src={this.state.imageUrl} class = "border-2 border-dark rounded-circle" alt=""/>
         <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tag.map(tag => <li key = {tag}>{tag}</li>)}
+        </ul>
         </React.Fragment>);
     }
 
