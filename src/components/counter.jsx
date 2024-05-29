59217143dc5b395a2c6f72776a7e91f33278fba1
abhi <abhijeetkc01@gmail.com>
2024-05-29 20:21:48 +0545

    reset button added but others need to be fixed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 class Counter extends Component {
 
     state = {
-        value: this.props.counter.value,
+
         imageUrl: `https://picsum.photos/200/200?random=${this.props.counter.id}`,
-        tag: ['stay delusional', 'stay fit', 'stay hard', 'stay hungry', 'stay foolish'],
-        allowNegative: false
+        tag: ['stay delusional', 'stay fit', 'stay hard', 'stay hungry', 'stay foolish'], 
     };
     
     style = { 
+
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -16,55 +16,53 @@ class Counter extends Component {
         padding: '2px'
     };
 
-    handleIncrement = product => {
-        console.log(product);
-        this.setState({value: this.state.value + 1});
-    };
-
-    handleDecrement = x => {
-        if (this.state.value > 0 || this.state.allowNegative)
-        this.setState ({value: this.state.value - 1});
-    };
-
-    toggleAllowNegative = () => {
-        this.setState({allowNegative: !this.state.allowNegative})
-    };
-
     render() {  
-        console.log('props', this.props);
+
         return (<React.Fragment>
+
         <img style = {this.style} src={this.state.imageUrl} className = "border-2 border-dark rounded-circle" alt=""/>
+
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+
         <button 
-        onClick={this.handleIncrement}
+        onClick={() => this.props.onIncrement(this.props.counter)}
         className="btn btn-success btn-sm"
         >Increment</button>
+
         <button 
-        onClick={this.handleDecrement}
+        onClick={() => this.props.onDecrement(this.props.counter)}
         className="btn-dark btn-sm m-2"
-        disabled={this.state.value === 0  && !this.state.allowNegative ? 'disabled' : ''}
+        disabled={this.props.counter.value === 0  && !this.props.counter.allowNegative ? 'disabled' : ''}
         >Decrement</button>
+
         <button onClick = {() => this.props.onDelete(this.props.counter.id)} className="btn-danger btn-sm mr-2">Delete</button>
+
         <input
             type = "checkbox"
-            onChange = {this.toggleAllowNegative}
-            checked = {this.state.allowNegative}
+            onChange = {() => this.props.onToggle(this.props.counter.allowNegative)}
+            checked = {this.props.counter.allowNegative}
         />
         Allow Negative Count
+
         <ul>
             {this.state.tag.map(tag => <li key = {tag}>{tag}</li>)}
         </ul>
+
         </React.Fragment>);
     }
 
     getBadgeClasses() {
+
         let classes ="badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
+
     }
     formatCount() {
-        const {value: count} = this.state;
+
+        const {value: count} = this.props.counter;
         return count === 0 ? 'Zero' : count;
+
     }
 }
  
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,25 +4,53 @@ import Counter from './counter';
 class Counters extends Component {
     state = { 
         counter: [
-            {id: 1, value: 0},
-            {id: 2, value: 1},
-            {id: 3, value: 1},
-            {id: 4, value: 3}
+            {id: 1, value: 0, allowNegative: false},
+            {id: 2, value: 1, allowNegative: false},
+            {id: 3, value: 1, allowNegative: false},
+            {id: 4, value: 3, allowNegative: false}
         ]
      };
 
-     handleDelete = del_handler => {
+    handleDelete = del_handler => {
         const counter = this.state.counter.filter(x => x.id !== del_handler);
         this.setState({counter});
         };
 
+    handleReset = () => {
+        const counter = this.state.counter.map(x => {
+            x.value = 0;
+            return x;
+        })
+        this.setState({counter});
+    };
+
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({value: this.state.value + 1});
+    };
+
+    handleDecrement = x => {
+        if (this.state.value > 0 || this.state.allowNegative)
+        this.setState ({value: this.state.value - 1});
+    };
+
+    toggleAllowNegative = () => {
+        this.setState({allowNegative: !this.state.allowNegative})
+    };
+
     render() { 
         return (
             <React.Fragment>
+                <button 
+                onClick={this.handleReset}
+                className="btn-primary btn-sm">Reset</button>
                 {this.state.counter.map(counters => 
                 (<Counter 
                     key = {counters.id}
                     onDelete = {this.handleDelete}
+                    onIncrement = {this.handleIncrement}
+                    onDecrement = {this.handleDecrement}
+                    onToggle = {this.toggleAllowNegative}
                     counter = {counters}     
                 />)
                 )}
